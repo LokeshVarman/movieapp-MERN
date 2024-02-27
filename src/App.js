e45f139b35f32b5e0b3aaca5a6eb106d16fe7e0a
LokeshVarman <lokeshvarmandev@gmail.com>
2024-02-27 15:23:22 +0530

    built an movie search app using imdb api

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import './App.css';
+import{useState, useEffect} from 'react';
+import SearchIcon from './search.svg';
+import MovieCard from './MovieCard';
+
+
+
+const API_URL=' http://www.omdbapi.com?apikey=2d7619d0'
+
+const App=()=> {
+const[movies,setMovies]=useState([]);
+const[searchterm,setsearchterm]=useState("");
+
+useEffect(()=>{
+  searchMovies('Batman')
+},[]);
+
+const searchMovies=async(title)=>{
+  const response=await fetch(`${API_URL}&s=${title}`);
+  const data=await response.json();
+  setMovies(data.Search);
+}
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Movie Search</h1>
+
+    <div className='search'>
+      <input
+      placeholder='Search for Movies'
+      value={searchterm}
+      onChange={(e)=>setsearchterm(e.target.value)}
+      />
+      <img
+      src={SearchIcon}
+      alt='search icon'
+      onClick={()=>searchMovies(searchterm)}
+      />
+    </div>
+
+{ movies?.length>0
+?
+(
+  <div className='container'>
+     {movies.map((movie)=>(
+      <MovieCard movie={movie}/>
+))}
+ </div>
+):
+
+(<div className='empty'>
+  <h2>No Movies Found</h2>
+  </div>  
+  )
+}
+
+   
     </div>
   );
 }
